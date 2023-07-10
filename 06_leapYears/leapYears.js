38c0ccc5fdb452e01008db6049f58149feaf1902
Heath Hile <heathhile@gmail.com>
2023-07-09 22:17:42 -0400

    upto leapyears, got parts working

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
+function leapYears(year) {
+    let result = year % 4 === 0 ? true : false;
 
-};
+    return result
+}
 
 // Do not edit below this line
+
 module.exports = leapYears;
