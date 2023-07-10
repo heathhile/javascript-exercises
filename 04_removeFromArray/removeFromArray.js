f72f953af66a332d84ca4c0bc9acb15a75228045
Heath Hile <heathhile@gmail.com>
2023-07-09 20:54:48 -0400

    up to item 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
 
-};
+    let array2 = [];
+    array.forEach(element => {
+        if (!args.includes(element)) {
+            array2.push(element);
+        }
+    });
 
+    return array2;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
